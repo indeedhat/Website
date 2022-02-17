80273e5b54250e9267bd74402d8867778978f3c0
techlm77 <dominicyoungjackson@gmail.com>
2022-02-17 20:11:10 +0000

    Add files via upload
    This used to work when my website was hosted on Firebase but now I have my own server at home and it's hosted on there.

---
+++ Digital Media/common.js
@@ -0,0 +1,27 @@
+function getRecaptchaMode() {
+  var config = parseQueryString(location.hash);
+  return config['recaptcha'] === 'invisible' ?
+      'invisible' : 'normal';
+}
+
+function getEmailSignInMethod() {
+  var config = parseQueryString(location.hash);
+  return config['emailSignInMethod'] === 'password' ?
+      'password' : 'emailLink';
+}
+
+function parseQueryString(queryString) {
+  if (queryString.length &&
+      (queryString.charAt(0) == '#' || queryString.charAt(0) == '?')) {
+    queryString = queryString.substring(1);
+  }
+  var config = {};
+  var pairs = queryString.split('&');
+  for (var i = 0; i < pairs.length; i++) {
+    var pair = pairs[i].split('=');
+    if (pair.length == 2) {
+      config[pair[0]] = pair[1];
+    }
+  }
+  return config;
+}
---
+++ Digital Media/js/index.js
@@ -0,0 +1,153 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC0qFtpgH1KXhXIjisXW_CigzYQfh97xQ4",
+  authDomain: "techlm77.firebaseapp.com",
+  databaseURL: "https://techlm77.firebaseio.com",
+  projectId: "techlm77",
+  storageBucket: "techlm77.appspot.com",
+  messagingSenderId: "191356451344",
+  appId: "1:191356451344:web:0faec2838a3be70c106895",
+  measurementId: "G-HT79GZNTQP"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.auth.Auth.Persistence.LOCAL;
+
+$("#btn-login").click(function()
+{
+  var email = $("#email").val();
+  var password = $("#password").val();
+  if (email != "" && password != "")
+  {
+    var result = firebase.auth().signInWithEmailAndPassword(email, password);
+    result.catch(function(error)
+    {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      window.alert("Message : " + errorMessage);
+    });
+  }
+  else
+  {
+    window.alert("Form is incomplete. Please fill out out all field.");
+  }
+});
+
+$("#btn-signup").click(function()
+{
+  var email = $("#email").val();
+  var password = $("#password").val();
+  var cPassword = $("#confirmPassword").val();
+  
+  if (email != "" && password != "" && cPassword != "")
+  {
+    if(password == cPassword)
+    {
+      var result = firebase.auth().createUserWithEmailAndPassword(email, password);
+    
+      result.catch(function(error)
+      {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        console.log(errorCode);
+        console.log(errorMessage);
+        
+        window.alert("Message : " + errorMessage);
+      });
+    }
+    else
+    {
+      window.alert("Password do not match with the Confirm Password");
+    }
+  }
+  else
+  {
+    window.alert("Form is incomplete. Please fill out out all field.");
+  }
+});
+
+$("#btn-resetPassword").click(function()
+{
+var auth = firebase.auth();
+var email = $("#email").val();
+
+if(email != "")
+{
+  auth.sendPasswordResetEmail(email).then(function()
+  {
+    window.alert("Email has been sent to your, please check and verify.");
+  })
+  .catch(function(error)
+  {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    
+    console.log(errorCode);
+    console.log(errorMessage);
+    
+    window.alert("Message : " + errorMessage);
+  });
+}
+else 
+{
+  window.alert("Please write your email first.");
+}
+});
+
+$("#btn-update").click(function()
+{
+var fName = $("#firstName").val();
+var sName = $("#secondName").val();
+var gender = $("#gender").val();
+var bio = $("#bio").val();
+
+var rootRef = firebase.database().ref().child("Users");
+var userID = firebase.auth().currentUser.uid;
+var usersRef = rootRef.child(userID);
+
+if(fName!="" && sName!="" && gender!="" && bio!="")
+{
+  var userData = {
+    "firstName": fName,
+    "secondName": sName,
+    "gender": gender,
+    "bio": bio
+
+  };
+
+  usersRef.set(userData, function(error)
+  {
+    if(error)
+    {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      
+      console.log(errorCode);
+      console.log(errorMessage);
+      
+      window.alert("Message : " + errorMessage);
+    }
+    else
+    {
+      window.location.href = "MainPage.html";
+    }
+  });
+}
+else
+{
+  window.alert("Please write your email first.");
+}
+});
+
+function switchView(view)
+{
+  $.get({
+    url:view,
+    cache:false,
+
+  })
+  .then(function(data){
+    $("#container").html(data);
+  });
+}
