def82d732941362fc18036e1b660938b4905dfed
techlm77 <dominicyoungjackson@gmail.com>
2022-02-21 05:31:10 +0000

    Update script.js

--- Ideas/script.js
+++ Ideas/script.js
@@ -15,12 +15,36 @@ function myFunction() {
 
   if (document.body.scrollTop > 1 * h || document.documentElement.scrollTop > 1 * h) {
     document.getElementById("div2").className = "div2";
+    document.getElementById("html").classList.remove('html1');
+    document.getElementById("html").classList.add('html');
+    document.getElementById("css").classList.remove('css1');
+    document.getElementById("css").classList.add('css');
+    document.getElementById("js").classList.remove('js1');
+    document.getElementById("js").classList.add('js');
+    document.getElementById("php").classList.remove('php1');
+    document.getElementById("php").classList.add('php');
   } else if (document.body.scrollTop < 1 * h || document.documentElement.scrollTop < 1 * h) {
     document.getElementById("div2").className = "div2-";
+    document.getElementById("html").classList.remove('html');
+    document.getElementById("html").classList.add('html1');
+    document.getElementById("css").classList.remove('css');
+    document.getElementById("css").classList.add('css1');
+    document.getElementById("js").classList.remove('js');
+    document.getElementById("js").classList.add('js1');
+    document.getElementById("php").classList.remove('php');
+    document.getElementById("php").classList.add('php1');
   }
 
   if (document.body.scrollTop > 1.5 * h || document.documentElement.scrollTop > 1.5 * h) {
     document.getElementById("div2").className = "div2-";
+    document.getElementById("html").classList.remove('html');
+    document.getElementById("html").classList.add('html1');
+    document.getElementById("css").classList.remove('css');
+    document.getElementById("css").classList.add('css1');
+    document.getElementById("js").classList.remove('js');
+    document.getElementById("js").classList.add('js1');
+    document.getElementById("php").classList.remove('php');
+    document.getElementById("php").classList.add('php1');
   }
 
   if (document.body.scrollTop > 2 * h || document.documentElement.scrollTop > 2 * h) {
@@ -43,13 +67,9 @@ function myFunction() {
     document.getElementById("div4").className = "div4-";
   }
 
-  if (document.body.scrollTop > 4 * h || document.documentElement.scrollTop > 4 * h) {
+  if (document.body.scrollTop > 3.35 * h || document.documentElement.scrollTop > 3.35 * h) {
     document.getElementById("div5").className = "div5";
   } else if (document.body.scrollTop < 1 * h || document.documentElement.scrollTop < 1 * h) {
-    document.getElementById("div5").className = "div5-";
-  }
-
-  if (document.body.scrollTop > 4.5 * h || document.documentElement.scrollTop > 4.5 * h) {
     document.getElementById("div5").className = "div5-";
   }
 }
